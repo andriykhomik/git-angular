817943a480e4a51572e512840c9c4503f263bf99
Vladimir <vyemshyn@elinext.com>
2022-02-18 00:34:23 +0200

    small refactor

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,29 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from "../../services/user.service";
-import {ActivatedRoute} from "@angular/router";
-import {User} from "../../interfaces";
+import { UserService } from '../../services/user.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../interfaces';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-
-  user!: User;
+  user$!: Observable<User>;
 
   constructor(
     private userService: UserService,
     private activatedRout: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRout.params.subscribe(login => {
-      console.log(login['username']);
-      this.userService.getUser(`https://api.github.com/users/${login['username']}`).subscribe(user=> {
-        this.user = user;
-      })
-    })
+    this.getUser();
   }
 
+  private getUser(): void {
+    this.activatedRout.params.subscribe((login: Params) => {
+      this.user$ = this.userService.getUser(login['username']);
+    });
+  }
 }
---
+++ src/app/interceptor/headers.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const req = request.clone({
+      setHeaders: {},
+    });
+
+    console.log(req);
+    return next.handle(req);
+  }
+}
